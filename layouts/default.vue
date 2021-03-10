4b9259264aee18e4afc0965b45af691715cf73d3
윤준현 <yoonjunhyeon@twentyoz.kr>
2021-03-10 15:15:42 +0900

    Header 꾸미기

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,80 @@
 <template>
   <a-config-provider>
-
-  <a-layout :style="{height: '100vh'}"
-  >
-    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        :default-selected-keys="['2']"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">
-          <nuxt-link to="/">
-            RT-Map
-          </nuxt-link>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <nuxt-link to="/database">
-            DataBase
-          </nuxt-link>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <nuxt-link to="/analytics">
-            Analytics
-          </nuxt-link>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <a-layout-content :style="{ padding: '0 20px', marginTop: '64px' }">
-      <div :style="{ background: '#fff', padding: '24px', height: '100%'}">
-        <Nuxt/>
-      </div>
-    </a-layout-content>
-    <a-layout-footer :style="{ textAlign: 'center' }">
-      <layout-footer/>
-    </a-layout-footer>
-  </a-layout>
+    <a-layout :style="{height: '100vh'}">
+      <a-layout-header class="header-style">
+        <a-row type="flex" justify="space-between">
+          <a-menu
+            theme="dark"
+            mode="horizontal"
+            :default-selected-keys="['2']"
+            :style="{ lineHeight: '40px', marginTop: '10px' }"
+          >
+            <a-menu-item key="null" disabled
+                         :style="{marginLeft: '10px', marginRight: '20px'}"
+            >
+              <div>
+                <a-icon class="logo-title"
+                        type="compass"
+                />
+                <span class="logo-title">Drone Web</span>
+              </div>
+            </a-menu-item>
+            <a-menu-item key="1" class="router-menu-item">
+              <nuxt-link to="/">
+                <a-icon type="environment"/>
+                RT-Map
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="2" class="router-menu-item router-menu-margin">
+              <nuxt-link to="/database">
+                <a-icon type="database"/>
+                DataBase
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="3" class="router-menu-item">
+              <nuxt-link to="/analytics">
+                <a-icon type="dashboard"/>
+                Analytics
+              </nuxt-link>
+            </a-menu-item>
+          </a-menu>
+          <div class="nav-etc">
+            <a-button type="primary" icon="user" />
+          </div>
+        </a-row>
+      </a-layout-header>
+      <a-layout-content :style="{ padding: '0 20px', marginTop: '84px' }">
+        <div :style="{ background: '#fff', padding: '20px', height: '100%'}">
+          <Nuxt/>
+        </div>
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }">
+        <layout-footer/>
+      </a-layout-footer>
+    </a-layout>
   </a-config-provider>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.logo-title {
+  color: white;
+  font-size: 20px;
+}
+.router-menu-item {
+ border-radius: 8px;
+}
+.router-menu-margin {
+  margin: 0 10px;
+}
+.header-style {
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+  padding: 0px;
+}
+.nav-etc {
+  margin-right: 20px;
+  margin-top: 16px;
+  line-height: 30px;
+}
 </style>
